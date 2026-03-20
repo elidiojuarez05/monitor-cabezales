40deaa52eb656cd0eeaab813e3ff9d3912e14675
elidiojuarez05 <elidiojuarez05@gmail.com>
2026-03-19 21:38:45 -0600

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,2 +1,35 @@
 import streamlit as st
-st.json(st.secrets)
+import gspread
+from google.oauth2.service_account import Credentials
+import pandas as pd
+
+st.title("📊 Dashboard Monitor")
+
+# Scopes
+scope = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+# Credenciales desde secrets
+creds = Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=scope
+)
+
+client = gspread.authorize(creds)
+
+# Abrir spreadsheet
+spreadsheet = client.open_by_url(
+    st.secrets["connections"]["gsheets"]["spreadsheet"]
+)
+
+# Leer hoja
+sheet = spreadsheet.worksheet("tests")  # ⚠️ cambia si tu hoja se llama distinto
+
+data = sheet.get_all_records()
+
+df = pd.DataFrame(data)
+
+# Mostrar
+st.dataframe(df)
