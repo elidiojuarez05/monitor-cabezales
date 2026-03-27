c5d59f64a2d47bdb642ee0cb45fcaa618f2ec2f3
Code By Juarez <elidiojuarez05@gmail.com>
2026-03-26 18:42:11 -0600

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -3,7 +3,7 @@ MACHINE_CONFIGS = {
     "VUTEK F4": {"cols": 48, "rows": 700, "x_start": 1, "x_end": 48},
     "VUTEK H5": {"cols": 28, "rows": 3200, "x_start": 1, "x_end": 28},
     "DURST 312": {"cols": 16, "rows": 4, "x_start": 20, "x_end": 800},
-    "DURST P10 PLUS": {"cols": 32, "rows": 460, "x_start": 1, "x_end": 302},
+    "DURST P10 PLUS": {"cols": 4, "rows": 30, "x_start": 1, "x_end": 302},
     "FAVUTEK ": {"cols": 4, "rows": 5, "x_start": 20, "x_end": 800},
     "MIMAKI": {"cols": 4, "rows": 1, "x_start": 35, "x_end": 612},
     "EPSON1": {"cols": 10, "rows": 1, "x_start": 20, "x_end": 800},
